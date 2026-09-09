d3fe3adc0674d6c061f3c24793b4a5e223b7de2b
julia-dm <yulia.2508dmitruk@gmail.com>
2026-09-09 10:56:34 +0200

    01-example

---
+++ Classe1/Yuliia/01-example.php
@@ -0,0 +1,31 @@
+<?php
+
+// la classe est ecrit en PascalCase
+// elle n'est pas un objet en tant que tel
+// mais plutot une fabrique à créer des objets
+class MyFirstClass{
+// Propriétés (var de la classe), plus souvant du camelCase 
+// public (accessible et modofiable en dehors de la classe)
+public string $name='';
+// privat (accessible et modofiable que dans la classe ou son instance)
+private string $surName='';
+// protected (accessible et modofiable que dans la classe ou son instance ET/OU dans ses enfants => "héritage")
+protected ?int $identifiant=null; // null par defaut, null ou int ?int
+// Constantes (Conteneurs invariables de la classe)
+const IS_ALIVE=true;//par defaut publique, typage autorisé à partir de PHP 8.3
+
+// Méthodes (fonctions de la classe)
+        // constructeur => passer des arguments à l'instance de classe 
+        // est toujours publique et utilise la méthode magique __construct(arg)
+        // est invoqué lors de l'instanciation (new)
+        public function __construct(
+
+    )
+        {
+            // self::class représent le nom de la class, donc de la fabrique 
+            echo "on a crée une instance de ".self::class;
+        }
+
+}
+// instanciation de $first
+$first=new MyFirstClass;
